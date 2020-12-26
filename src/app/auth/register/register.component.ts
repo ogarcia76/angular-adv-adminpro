9c0370f06c821c2b48b523f0f8661773d27a9391
Oscar Garcia <ogarcia@portel.es>
2020-12-26 10:18:25 +0100

    primeros pasos angular

---
+++ src/app/auth/register/register.component.ts
@@ -0,0 +1,16 @@
+import { registerLocaleData } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: [ './register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
