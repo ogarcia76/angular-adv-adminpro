abb2cd1343f89113a797ccf890a4a2ccfd3b6dc3
Oscar Garcia <ogarcia@portel.es>
2021-01-03 18:34:18 +0100

    Fin seccion 4 - Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
-import { ProgressComponent } from './pages/progress/progress.component';
+import { PagesRoutingModule } from './pages/pages.routing';
+import { AuthModule } from './auth/auth.module';
+
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 const routes: Routes = [
-  { 
-    path: '', 
-    component: PagesComponent,
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'grafica1', component: Grafica1Component },
-      {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-    ]
-  },
-  
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
-  {path: '**', component: NopagefoundComponent}
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '**', component: NopagefoundComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    PagesRoutingModule,
+    AuthModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
